ba2ca7bad5202eaadab1c92d80f75a3694a07e16
Logan W <soslylw@gmail.com>
2023-11-11 14:36:56 -0500

    Changed search bar and background colour

--- namer_app/lib/communities.dart
+++ namer_app/lib/communities.dart
@@ -36,67 +36,72 @@ class _CommunitiesHomePageState extends State<CommunitiesHomePage> {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    final theme = Theme.of(context);
 
-    return Column(
-      // mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        //Something for the top bar
-        DecoratedBox(
-          decoration: BoxDecoration(border: Border.all()),
-          child: SizedBox(
-            height: MediaQuery.of(context).size.height / 8,
-          ),
-        ),
-        //Title
-        Text('Browse Communities'),
-        //Search Bar
-        SearchAnchor(
-            builder: (BuildContext context, SearchController controller) {
-          return SearchBar(
-            controller: controller,
-            padding: const MaterialStatePropertyAll<EdgeInsets>(
-                EdgeInsets.symmetric(horizontal: 16.0)),
-            onTap: () {
-              controller.openView();
-            },
-            onChanged: (_) {
-              controller.openView();
-            },
-            leading: const Icon(Icons.search),
-          );
-        }, suggestionsBuilder:
-                (BuildContext context, SearchController controller) {
-          return List<ListTile>.generate(5, (int index) {
-            final String item = 'item $index';
-            return ListTile(
-              title: Text(item),
-              onTap: () {
-                setState(() {
-                  controller.closeView(item);
-                });
-              },
-            );
-          });
-        }),
-        //Items (Scrollable)
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: ListView.builder(
-              itemCount: communities.length,
-              shrinkWrap: true,
-              padding: EdgeInsets.only(top: 16),
-              physics: NeverScrollableScrollPhysics(),
-              itemBuilder: (context, index) {
-                return Card(
-                    child: ListTile(
-                        leading: communities[index].icon,
-                        title: Text(communities[index].name)));
-              },
+    return Container(
+      color: theme.colorScheme.background,
+      child: Column(
+        children: [
+          //Something for the top bar
+          DecoratedBox(
+            decoration: BoxDecoration(border: Border.all()),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height / 8,
             ),
           ),
-        )
-      ],
+          //Title
+          Text('Browse Communities'),
+          //Search Bar
+          SearchAnchor(
+              builder: (BuildContext context, SearchController controller) {
+            return SearchBar(
+              backgroundColor: MaterialStateProperty.all(Colors.white),
+              controller: controller,
+              padding: const MaterialStatePropertyAll<EdgeInsets>(
+                  EdgeInsets.symmetric(horizontal: 16.0)),
+              onTap: () {
+                controller.openView();
+              },
+              onChanged: (_) {
+                controller.openView();
+              },
+              leading: const Icon(Icons.search),
+            );
+          }, suggestionsBuilder:
+                  (BuildContext context, SearchController controller) {
+            return List<ListTile>.generate(5, (int index) {
+              final String item = 'item $index';
+              return ListTile(
+                title: Text(item),
+                onTap: () {
+                  setState(() {
+                    controller.closeView(item);
+                  });
+                },
+              );
+            });
+          }),
+          //Items (Scrollable)
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: ListView.builder(
+                itemCount: communities.length,
+                shrinkWrap: true,
+                padding: EdgeInsets.only(top: 16),
+                physics: NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return Card(
+                      color: theme.colorScheme.primary,
+                      child: ListTile(
+                          leading: communities[index].icon,
+                          title: Text(communities[index].name)));
+                },
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
